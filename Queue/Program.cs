eb9138bdd056bc3ca4537844effc8faa934dd48b
glennmateus <glennmateus@gmail.com>
2022-04-07 08:57:34 -0300

    Csharp collections finished

--- Queue/Program.cs
+++ Queue/Program.cs
@@ -1,1 +1,35 @@
-﻿
+﻿public class Queues
+{
+	static Queue<string> pedagio = new Queue<string>();
+	/*
+	* Queues implement the "First in First out" methodology
+	* Enqueue ::  Add a new item to queue
+	* Dequeue ::  Remove the first item from queue
+	* Peek	  ::  Take a look into the next item to remove from queue
+	* https://docs.microsoft.com/en-us/dotnet/api/system.collections.generic.queue-1?view=net-6.0
+	*/
+	static void Main()
+	{
+		AddToQueue("van");
+		AddToQueue("kombi");
+		AddToQueue("fusca");
+		RemoveFromQueue();
+		RemoveFromQueue();
+		RemoveFromQueue();
+	}
+
+	static void AddToQueue(string vehicle)
+	{
+		pedagio.Enqueue(vehicle);
+		System.Console.WriteLine(string.Join("-", pedagio));
+	}
+
+	static void RemoveFromQueue()
+	{
+		if (pedagio.Any())
+		{
+			pedagio.Dequeue();
+			System.Console.WriteLine(string.Join("-", pedagio));
+		}
+	}
+}
--- Stack/Program.cs
+++ Stack/Program.cs
@@ -1,5 +1,12 @@
 ﻿public class Program
 {
+	/*
+	* Stacks implement the "First in Last out" methodology
+	* Push :: Inserts an item to Stack
+	* Pop  :: Removes an item from Stack
+	* Peek :: Returns the item at the top of the Stack
+	* https://docs.microsoft.com/en-us/dotnet/api/system.collections.stack?view=net-6.0
+	*/
 	public static void Main()
 	{
 		var browser = new Browser();
