b23b612d7f313db1eeb2d6d10d01a60bf0de0ee6
glennmateus <glennmateus@gmail.com>
2022-04-11 07:56:37 -0300

    Contract | Inheritance explanation

--- ByteBank.Models/Models/AuthenticatedUsers/AuthenticatedUsers.cs
+++ ByteBank.Models/Models/AuthenticatedUsers/AuthenticatedUsers.cs
@@ -1,9 +1,15 @@
 namespace Models;
 
+/*
+! Inheritance :: A class inherits a "mother class" (Only one)
+! Contract :: A class can implement multiple Interfaces
+! Hierarchy of implementations	:: First comes the Inheritance, and after all Contracts
+! Ex: AuthenticatedUsers : Inherits Employee, Implements IAuthenticateUser contract
+*/
 public abstract class AuthenticatedUsers : Employee, IAuthenticateUser
 {
-    private AuthenticationHelper _authenticationHelper = new AuthenticationHelper();
-    public string Password { get; set; }
-    public AuthenticatedUsers(double salary, string cpf) : base(salary, cpf) { }
-    public bool Authenticate(string password) => _authenticationHelper.PasswordCompare(Password, password);
+	private AuthenticationHelper _authenticationHelper = new AuthenticationHelper();
+	public string Password { get; set; }
+	public AuthenticatedUsers(double salary, string cpf) : base(salary, cpf) { }
+	public bool Authenticate(string password) => _authenticationHelper.PasswordCompare(Password, password);
 }
